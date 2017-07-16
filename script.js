c38c91c0a6acd98a760bc8801a21f26855168527
will ye <williamyeny@gmail.com>
2017-07-16 02:01:25 -0400

    added loop

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 var tradeIds = $(".trade:not(trade-closed)").attr("data-tradeid");
-console.log(tradeIds);
+var minSleep = 1800000; //minimum time to sleep between bumps in milliseconds, 1800000 = 32 min
+var maxSleep = 2400000; //maximum time to sleep between bumps in milliseconds, 2400000 = 40 min
+
+//start loop
+setTimeout(function() {
+
+}, getRandomInt(minSleep, maxSleep));
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//
+// $.post("http://www.tf2outpost.com/api/core",
+// {
+//     name: "Donald Duck",
+//     city: "Duckburg"
+// },
+// function(data, status){
+//     alert("Data: " + data + "\nStatus: " + status);
+// });
